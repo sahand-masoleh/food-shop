3e7271c9389b63b86f4ea0f48199fc0f6446bb12
Sahand <sahand.masoleh@gmail.com>
2022-11-22 23:46:15 +0100

    feat: lables and links for hero images

--- components/Home/Carousel/Carousel.styles.tsx
+++ components/Home/Carousel/Carousel.styles.tsx
@@ -29,25 +29,44 @@ export const Div_Carousel = styled.div`
 
 /* each image */
 export const Div_ClippedImage = styled.div`
-	pointer-events: none;
-	height: 38rem;
-	width: 38rem;
 	flex-shrink: 0;
 	margin: 0 0.5rem;
+	height: 38rem;
+	width: 38rem;
 	position: relative;
-	pointer-events: none;
+	z-index: 1;
 	user-select: none;
 
-	& > img {
-		z-index: 10;
-		mask: inherit;
+	& > a > img {
+		height: 100%;
 		object-fit: cover;
-		object-position: center;
 		clip-path: var(--clip);
 	}
 
 	& > svg {
+		z-index: -1;
+		inset: 0;
 		position: absolute;
 		stroke: black;
+	}
+
+	& > span {
+		opacity: 0;
+		transition: opacity var(--transition-duration) linear;
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		translate: -50% -50%;
+		background-color: var(--white);
+		padding: 0.33ch 1ch;
+		border-radius: 2rem;
+		text-transform: capitalize;
+		font-size: 1.6rem;
+		font-weight: 600;
+		pointer-events: none;
+	}
+
+	&.hovered > span {
+		opacity: 1;
 	}
 `;
--- components/Home/Carousel/ImageContainer.tsx
+++ components/Home/Carousel/ImageContainer.tsx
@@ -1,8 +1,20 @@
+/*
+	The Carousel
+	Each set of images are repeated twice, each set has an intersection observer;
+	The first set on the first image, and the second set on the last image.
+	Upon intersection of the images, the carousel will scroll to the same image in the middle.
+*/
+
 import Image from "next/image";
-import { useEffect, useRef } from "react";
+import Link from "next/link";
+import React, { MutableRefObject, useEffect, useRef, useState } from "react";
 import { clipIterator } from "./SVGDefs";
 import * as s from "./Carousel.styles";
 import { DBProductable } from "@/types/Product";
+
+/* 
+	EACH SET OF HERO IMAGES
+*/
 
 export type Hero = Pick<DBProductable, "id" | "name"> & { hero: string };
 
@@ -17,43 +29,29 @@ function ImageContainer({
 	order,
 	handleIntersection,
 }: ImageContainerable) {
-	const ref = useRef<HTMLDivElement>();
-	const observer = useRef<IntersectionObserver>();
-
-	useEffect(() => {
-		observer.current = new IntersectionObserver((e) =>
-			handleIntersection(e[0])
-		);
-		observer.current.observe(ref.current);
-
-		return () => observer.current.disconnect();
-	}, []);
-
+	/* detect which end of the line the observee is */
 	function detectEnd(i: number) {
 		if (i === 0 && order === "first") return "left";
 		if (i === heroes.length - 1 && order === "second") return "right";
 	}
 
+	/* an iterator that spits out SVG clippers in order */
 	const frames = clipIterator();
+
+	/* map of hero images */
 	const heroesMap = heroes.map((hero, i) => {
 		const end = detectEnd(i);
 		const frame = frames.next().value;
 		return (
-			<s.Div_ClippedImage
+			<ImageWrapper
 				key={hero.id}
-				ref={end ? ref : null}
-				data-end={end}
-				style={
-					{
-						"--clip": `url(#${frame}-clip)`,
-					} as React.CSSProperties
-				}
-			>
-				<Image src={hero.hero} alt={hero.name} fill />
-				<svg viewBox="0 0 1 1">
-					<use href={`#${frame}`} />
-				</svg>
-			</s.Div_ClippedImage>
+				/* only put in the ref if in desired position */
+				end={end}
+				handleIntersection={end ? handleIntersection : null}
+				frame={frame}
+				name={hero.name}
+				hero={hero.hero}
+			/>
 		);
 	});
 
@@ -61,3 +59,72 @@ function ImageContainer({
 }
 
 export default ImageContainer;
+
+/*
+	EACH Clipped IMAGE
+*/
+
+interface ImageWrapperable {
+	end: "left" | "right";
+	frame: string;
+	name: string;
+	hero: string;
+	handleIntersection?: (entry: IntersectionObserverEntry) => void;
+}
+
+function ImageWrapper({
+	end,
+	frame,
+	name,
+	hero,
+	handleIntersection,
+}: ImageWrapperable) {
+	/* intersection observer is stored in a ref*/
+	const observer = end && useRef<IntersectionObserver>();
+	/* the ref holding the observee */
+	const ref = end && useRef<HTMLDivElement>();
+
+	useEffect(() => {
+		/* on first mount, instantiate the observer */
+		/* observe the object in the ref only if it is at the end */
+		if (end) {
+			observer.current = new IntersectionObserver((e) =>
+				handleIntersection(e[0])
+			);
+			observer.current.observe(ref.current);
+		}
+
+		/* clean up before unmounting */
+		return () => end && observer.current.disconnect();
+	}, []);
+
+	/* detect the hover state to show the title */
+	const [isHovered, setIsHoeverd] = useState(false);
+	function handleHover(bool: boolean) {
+		setIsHoeverd(bool);
+	}
+
+	return (
+		<s.Div_ClippedImage
+			className={isHovered ? "hovered" : null}
+			onMouseEnter={() => handleHover(true)}
+			onMouseLeave={() => handleHover(false)}
+			ref={end && ref}
+			/* the intersection callback looks at data-end to decide where to scroll to */
+			data-end={end}
+			style={
+				{
+					"--clip": `url(#${frame}-clip)`,
+				} as React.CSSProperties
+			}
+		>
+			<Link href={`/products/${name}`}>
+				<Image src={hero} alt={name} width={640} height={640} />
+			</Link>
+			<svg viewBox="0 0 1 1">
+				<use href={`#${frame}`} />
+			</svg>
+			<span>{name}</span>
+		</s.Div_ClippedImage>
+	);
+}
